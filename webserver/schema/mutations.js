b8d2683333e8e29f570018dd6b167fe5a882f923
koyamot <koyamot@gmail.com>
2022-01-12 15:00:41 -0800

    edit gql  mutations

--- webserver/schema/mutations.js
+++ webserver/schema/mutations.js
@@ -17,8 +17,33 @@ const mutation = new GraphQLObjectType({
         amount: { type: GraphQLFloat }
       },
       /* eslint-disable-next-line camelcase */
-      resolve (parentValue, { user_id, description, merchant_id, debit, credit, amount }) {
+      resolve (_, { user_id, description, merchant_id, debit, credit, amount }) {
         return (new TransactionModel({ user_id, description, merchant_id, debit, credit, amount })).save()
+      }
+    },
+    editTransaction: {
+      type: TransactionType,
+      args: {
+        id: { type: GraphQLString },
+        user_id: { type: GraphQLString },
+        description: { type: GraphQLString },
+        merchant_id: { type: GraphQLString },
+        debit: { type: GraphQLBoolean },
+        credit: { type: GraphQLBoolean },
+        amount: { type: GraphQLFloat }
+      },
+      /* eslint-disable-next-line camelcase */
+      resolve (_, { id, user_id, description, merchant_id, debit, credit, amount }) {
+        return (TransactionModel.findByIdAndUpdate(id, { user_id, description, merchant_id, debit, credit, amount }))
+      }
+    },
+    deleteTransaction: {
+      type: TransactionType,
+      args: {
+        id: { type: GraphQLString }
+      },
+      resolve (_, data) {
+        return (TransactionModel.findByIdAndDelete(data.id))
       }
     }
   }
