0cdef014a4230ad2dac2bd2faf3774ccf8f78c70
koyamot <koyamot@gmail.com>
2022-01-14 10:44:07 -0800

    updated roman numeral switch

--- webapp/src/components/roman/RomanTest.js
+++ webapp/src/components/roman/RomanTest.js
@@ -4,8 +4,9 @@ const RomanTest = str => {
   var result = ''
   var lookup = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 }
   for (i in regexStr) {
+    var newNum = regexStr[i]
     if (!isNaN(regexStr[i])) {
-      var num = Number(regexStr[i])
+      var num = Number(newNum)
       for (var x in lookup) {
         while (num >= lookup[x]) {
           result += x
@@ -13,7 +14,7 @@ const RomanTest = str => {
         }
       }
     } else {
-      result += regexStr[i]
+      result += newNum
     }
   }
 
