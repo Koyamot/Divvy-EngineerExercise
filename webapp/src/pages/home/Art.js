1b1ddd4203b85be5b3e96eb4e28b10b301b99d7c
koyamot <koyamot@gmail.com>
2022-01-18 16:12:56 -0800

    styles for art page

--- webapp/src/pages/home/Art.js
+++ webapp/src/pages/home/Art.js
@@ -9,6 +9,7 @@ export default function Art () {
       <h1>{t('shameless_plug')}</h1>
       <h2>I create NFTs under the alias <a href='https://opensea.io/atinyglitch'>ATinyGlitch</a></h2>
       <p>The two gifs below I created using Procreate, and Adobe After Effects. My goal is to evoke specific emotions through my art.</p>
+      <p>I also tattoo, paint, I enjoy watering the yard, and doing other regular human things.</p>
       <div className='art-container'>
         <img
           alt='rainy daze'
@@ -22,13 +23,18 @@ export default function Art () {
 }
 
 const art = css`
-  background: none;
-  display: grid;
+background: #ffedff;
+padding: 32px;
+display: grid;
+align-items: center;
+border-radius: 16px;
+box-shadow: 2px 2px 8px 0 rgb(0 0 0 / 20%);
 
   .art-container {
-    display: grid;
-    grid-template-columns: auto auto auto;
+    margin: 0 auto;
+    margin-top: 40px;
     img {
+      padding: 16px;
       width: 400px;
     }
   }
